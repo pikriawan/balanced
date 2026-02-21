0902d4feb52e0f5dd1c8528b6b4240759f27124f
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-02-21 14:53:20 +0700

    Add the ability to save the signed in users into the database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
+import { eq } from "drizzle-orm";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import db from "../../../../lib/db";
+import { users } from "../../../../db/schema";
 
 const handler = NextAuth({
     providers: [
@@ -7,7 +10,28 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
-    ]
+    ],
+    callbacks: {
+        async signIn({ user }) {
+            const existingUsers = await db
+                .select()
+                .from(users)
+                .where(eq(users.email, user.email));
+            const exists = existingUsers.length > 0;
+
+            if (!exists) {
+                await db
+                    .insert(users)
+                    .values({
+                        name: user.name,
+                        email: user.email,
+                        image: user.image
+                    });
+            }
+
+            return true;
+        }
+    }
 });
 
 export { handler as GET, handler as POST };
