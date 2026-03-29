29ed13e9ece763b9d845f4ef69ec6a63bc230ba5
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-03-29 16:44:24 +0700

    Create switch component

--- components/ui/switch.jsx
+++ components/ui/switch.jsx
@@ -1,24 +1,35 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
 
-export default function Switch({ className, isEnabled: defaultIsEnabled, ...props }) {
-    const [isEnabled, setIsEnabled] = useState(defaultIsEnabled);
-    const inputRef = useRef();
+export default function Switch({ className, defaultIsEnabled = false, ...props }) {
+    const [value, setValue] = useState(defaultIsEnabled ? "on" : "off");
 
     return (
         <>
             <input
                 type="hidden"
-                value={isEnabled}
-                ref={inputRef}
-                onChange={() => setIsEnabled(inputRef.current.value)}
+                value={value}
+                onChange={(event) => setValue(event.target.value)}
                 {...props}
             />
-            <span className={cn("w-10 h-6 relative rounded-full bg-neutral-800 transition-[background-color]", isEnabled && "bg-neutral-50", className)} onClick={() => setIsEnabled((isEnabled) => !isEnabled)}>
-                <span className={cn("w-4 h-4 absolute top-1 left-1 rounded-full bg-neutral-50 transition-[left,background-color]", isEnabled && "left-[calc(100%-1.25rem)] bg-neutral-950")}></span>
-            </span>
+            <button
+                type="button"
+                className={cn(
+                    "w-10 h-6 relative rounded-full bg-neutral-800 transition-[background-color]",
+                    value === "on" && "bg-neutral-50",
+                    className
+                )}
+                onClick={() => setValue((value) => value === "on" ? "off" : "on")}
+            >
+                <span
+                    className={cn(
+                        "w-4 h-4 absolute top-1 left-1 rounded-full bg-neutral-50 transition-[left,background-color]",
+                        value === "on" && "left-[calc(100%-1.25rem)] bg-neutral-950"
+                    )}
+                />
+            </button>
         </>
     );
 }
