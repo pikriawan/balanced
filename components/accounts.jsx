1d1f385adc29433c0e36a1de3b32a7a239aea1c0
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-04-01 22:08:43 +0700

    Add overflow auto to the accounts table

--- components/accounts.jsx
+++ components/accounts.jsx
@@ -9,31 +9,33 @@ import {
 
 export default function Accounts({ companyId, accounts }) {
     return (
-        <Table className="w-full">
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Kode akun</TableHead>
-                    <TableHead>Nama akun</TableHead>
-                    <TableHead>Tipe akun</TableHead>
-                    <TableHead>Aksi</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {accounts.length > 0 && accounts.map((account) => (
-                    <TableRow key={account.id}>
-                        <TableCell>
-                            {account.code}
-                        </TableCell>
-                        <TableCell>
-                            {account.name}
-                        </TableCell>
-                        <TableCell>
-                            {account.type}
-                        </TableCell>
-                        <TableCell />
+        <div className="w-full overflow-auto">
+            <Table className="w-full">
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Kode akun</TableHead>
+                        <TableHead>Nama akun</TableHead>
+                        <TableHead>Tipe akun</TableHead>
+                        <TableHead>Aksi</TableHead>
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                </TableHeader>
+                <TableBody>
+                    {accounts.length > 0 && accounts.map((account) => (
+                        <TableRow key={account.id}>
+                            <TableCell>
+                                {account.code}
+                            </TableCell>
+                            <TableCell>
+                                {account.name}
+                            </TableCell>
+                            <TableCell>
+                                {account.type}
+                            </TableCell>
+                            <TableCell />
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
     );
 }
