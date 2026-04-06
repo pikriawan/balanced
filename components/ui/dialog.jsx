bb423d2ca317990b41864c95395e5698e1f39e7f
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-04-06 09:13:36 +0700

    Render dialog content in portal to prevent layout inheritance issues

--- components/ui/dialog.jsx
+++ components/ui/dialog.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { createContext, useContext, useState } from "react";
+import { createPortal } from "react-dom";
 import { cn } from "@/lib/utils";
 
 export const DialogContext = createContext();
@@ -38,12 +39,13 @@ export function DialogClose({ children, ...props }) {
 export function DialogContent({ className, children, ...props }) {
     const { isShow } = useContext(DialogContext);
 
-    return isShow && (
+    return isShow && createPortal(
         <>
             <DialogClose className="w-full h-full fixed top-0 left-0 bg-neutral-950 opacity-25 z-1" />
             <div className={cn("w-[calc(100%-2rem)] fixed top-4 left-1/2 transform-[translateX(-50%)] p-4 border border-neutral-800 rounded-2xl bg-neutral-950 z-2 md:w-sm md:top-1/2 md:left-1/2 md:transform-[translate(-50%,-50%)]", className)} {...props}>
                 {children}
             </div>
-        </>
+        </>,
+        document.body
     );
 }
