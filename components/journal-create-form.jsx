bdf53d728cabb3365cbbe860341379a583ee37f7
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-04-19 10:46:03 +0700

    Update create journal form

--- components/journal-create-form.jsx
+++ components/journal-create-form.jsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Plus, Trash2 } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { createJournal } from "@/actions/journal";
 import Button from "@/components/ui/button";
 import ButtonLink from "@/components/ui/button-link";
 import { Field, FieldLabel } from "@/components/ui/field";
@@ -24,20 +25,55 @@ export default function JournalCreateForm({ companyId, accounts }) {
             credit: ""
         }
     ]);
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(null);
+    const autoFocusRef = useRef(null);
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const journalLinesString = JSON.stringify(journalLines.map((journalLine) => {
+            return {
+                name: journalLine.name,
+                debit: journalLine.debit,
+                credit: journalLine.credit
+            };
+        }));
+        formData.append("journalLines", journalLinesString);
+
+        setIsPending(true);
+        setError(null);
+
+        const response = await createJournal(companyId, formData);
+
+        setIsPending(false);
+
+        if (!response.success) {
+            setError(response.error);
+        }
+    }
+
+    useEffect(() => {
+        if (error) {
+            autoFocusRef.current.focus();
+            autoFocusRef.current.select();
+        }
+    }, [error]);
 
     return (
-        <div className="w-full flex flex-col items-start gap-4">
+        <form className="w-full flex flex-col items-start gap-4" onSubmit={onSubmit}>
             <Field className="w-full">
-                <FieldLabel htmlFor="journalCreate_date">Tanggal</FieldLabel>
-                <TextField className="w-full max-w-3xs" id="journalCreate_date" type="date" />
+                <FieldLabel htmlFor="journalCreate_date" ref={autoFocusRef}>Tanggal</FieldLabel>
+                <TextField className="w-full max-w-3xs" id="journalCreate_date" name="date" type="date" />
             </Field>
             <Field className="w-full">
                 <FieldLabel htmlFor="journalCreate_number">Nomor jurnal</FieldLabel>
-                <TextField className="w-full max-w-3xs" id="journalCreate_number" placeholder="JU00001" />
+                <TextField className="w-full max-w-3xs" id="journalCreate_number" name="number" placeholder="JU00001" />
             </Field>
             <Field className="w-full">
                 <FieldLabel htmlFor="journalCreate_description">Keterangan</FieldLabel>
-                <TextArea className="w-full max-w-3xs" id="journalCreate_description" placeholder="Keterangan" />
+                <TextArea className="w-full max-w-3xs" id="journalCreate_description" name="description" placeholder="Keterangan" />
             </Field>
             <div className="w-full p-1 overflow-x-auto">
                 <div className="w-4xl flex flex-col gap-2">
@@ -91,10 +127,18 @@ export default function JournalCreateForm({ companyId, accounts }) {
                 <Plus size={16} />
                 Tambah baris baru
             </Button>
-            <div className="flex items-center gap-4">
-                <ButtonLink href={`/companies/${companyId}/journals`} variant="outlined">Batal</ButtonLink>
-                <Button>Simpan jurnal</Button>
+            <div className="grid grid-cols-2 gap-4">
+                <ButtonLink className="w-full justify-center" href={`/companies/${companyId}/journals`} variant="outlined" disabled={isPending}>
+                    <span className="truncate">
+                        Batal
+                    </span>
+                </ButtonLink>
+                <Button className="w-full justify-center truncate" disabled={isPending}>
+                    <span className="truncate">
+                        Simpan jurnal
+                    </span>
+                </Button>
             </div>
-        </div>
+        </form>
     );
 }
